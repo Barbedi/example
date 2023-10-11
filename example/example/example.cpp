3b2f7a7b927429d616891c7441f68df23480a9b3
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-10-11 11:46:03 +0200

    2

--- example/example/example.cpp
+++ example/example/example.cpp
@@ -4,18 +4,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+
     class lista {
         int data;
         lista* prev;
         lista* next;
+        
+       
+        
     public:
+        unsigned c; // zerujemy licznik
         lista(int aa) :data(aa), prev(nullptr), next(nullptr){}//konstr
         ~lista(void){}//destr
 
-        void dodajStart();
-        void dodajEnd();
+        void dodajStart(int aa);
+        void dodajEnd(int aa);
         void dodajWskaIndex();
         void usunStart();
         void usunEnd();
@@ -28,7 +31,39 @@ int main()
 
         
     };
-}
+
+    void lista::dodajStart(int aa)
+    {
+       
+    }
+    void lista::dodajEnd(int aa)
+    {
+        lista* newNode = new lista(aa);
+        if (!next) {
+            next = newNode;
+            newNode->prev = this;
+        }
+        else {
+            lista* current = this;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = newNode;
+            newNode->prev = current;
+        }
+    }
+    
+        void lista::wyswietl()
+    {
+            lista* current = this;
+            while (current) {
+                cout << current->data << "";
+                current = current->next;
+            }
+    }
+
+
+
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
 // Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
@@ -40,3 +75,16 @@ int main()
 //   4. Użyj okna Lista błędów, aby zobaczyć błędy
 //   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
 //   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+
+    int main() {
+        lista list1(0);
+       
+        list1.dodajStart(12);
+        list1.dodajEnd(3);
+        
+     
+        
+        list1.wyswietl();
+      
+    }
+
